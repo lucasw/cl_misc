170b2bae418c4020647a2fc441f2b68c0a4d7953
lucasw <lucasw_backwards@gmail.com>
2014-07-22 20:34:21 -0700

    Now reading in same hello program from cl file, need to have it do something next.

--- src/cl_misc.cpp
+++ src/cl_misc.cpp
@@ -3,59 +3,66 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-
-// Later this will be read in from a file
-const char * helloStr  = "__kernel void "
-                         "hello(void) "
-                         "{ "
-                         "  "
-                         "} ";
+#include <fstream>
 
 int main(void)
 {
-   cl_int err = CL_SUCCESS;
-   try {
+  cl_int err = CL_SUCCESS;
+  try {
 
-     std::vector<cl::Platform> platforms;
-     cl::Platform::get(&platforms);
-     if (platforms.size() == 0) {
-         std::cout << "Platform size 0\n";
-         return -1;
-     }
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    if (platforms.size() == 0) {
+      std::cout << "Platform size 0\n";
+      return EXIT_FAILURE;
+    }
 
-     cl_context_properties properties[] = 
-        { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0};
-     cl::Context context(CL_DEVICE_TYPE_CPU, properties); 
+    cl_context_properties properties[] = 
+    { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0};
+    cl::Context context(CL_DEVICE_TYPE_CPU, properties); 
 
-     std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+    std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
-     cl::Program::Sources source(1,
-         std::make_pair(helloStr, strlen(helloStr)));
-     cl::Program program_ = cl::Program(context, source);
-     program_.build(devices);
+    std::ifstream cl_file;
+    cl_file.open("misc.cl"); //, std::ios::in);
+    if (!cl_file.is_open()) {
+      return EXIT_FAILURE;
+    }
+    std::string big_line;
+    std::string line;
+    while (std::getline(cl_file, line)) {
+      big_line += line + "\n"; 
+    }
+    //std::cout << big_line << std::endl;
 
-     cl::Kernel kernel(program_, "hello", &err);
+    cl::Program::Sources source(1,
+        std::make_pair(big_line.c_str(), strlen( big_line.c_str() )));
+    cl::Program program_ = cl::Program(context, source);
+    program_.build(devices);
 
-     cl::Event event;
-     cl::CommandQueue queue(context, devices[0], 0, &err);
-     queue.enqueueNDRangeKernel(
-         kernel, 
-         cl::NullRange, 
-         cl::NDRange(4,4),
-         cl::NullRange,
-         NULL,
-         &event); 
+    cl::Kernel kernel(program_, "hello", &err);
 
-     event.wait();
-   }
-   catch (cl::Error err) {
-      std::cerr 
-         << "ERROR: "
-         << err.what()
-         << "("
-         << err.err()
-         << ")"
-         << std::endl;
-   }
+    cl::Event event;
+    cl::CommandQueue queue(context, devices[0], 0, &err);
+    queue.enqueueNDRangeKernel(
+        kernel, 
+        cl::NullRange, 
+        cl::NDRange(4,4),
+        cl::NullRange,
+        NULL,
+        &event); 
+
+    event.wait();
+  }
+  catch (cl::Error err) {
+    std::cerr 
+      << "ERROR: "
+      << err.what()
+      << "("
+      << err.err()
+      << ")"
+      << std::endl;
+    return EXIT_FAILURE;
+  }
   return EXIT_SUCCESS;
 }
---
+++ src/misc.cl
@@ -0,0 +1,5 @@
+__kernel void hello(void) 
+{ 
+
+};
+
