048b61c0bb14e51fbf5d2028b13e5249de805ede
lucasw <lucasw_backwards@gmail.com>
2014-07-23 21:21:29 -0700

    for loop is slow around size 20000 (1 second), need to read in real image data

--- src/cl_misc.cpp
+++ src/cl_misc.cpp
@@ -61,91 +61,85 @@ int main(void)
     }
       std::cout << std::endl;
     }
-
-    cl::Image2D cl_image = cl::Image2D(
-        context,
-         CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-         cl::ImageFormat(CL_R, CL_UNSIGNED_INT8),  // single channel
-         wd,
-         ht,
-         0,
-         (void*) &im, // some random memory
-         &err
-         );
-
-    unsigned char im_out[wd * ht];
+    
+    //unsigned char im_out[wd * ht];
 
     cl::size_t<3> origin;
     origin[0] = 0; origin[1] = 0, origin[2] = 0;
     cl::size_t<3> region;
     region[0] = wd; region[1] = ht; region[2] = 1;
+    
+    cl::Kernel kernel(program_, "hello", &err);
+
+    //bool ping = true;
+
+    cl::Image2D cl_image = cl::Image2D(
+        context,
+        CL_MEM_READ_ONLY, // | CL_MEM_COPY_HOST_PTR,
+        cl::ImageFormat(CL_R, CL_UNSIGNED_INT8),  // single channel
+        wd,
+        ht,
+        0,
+        NULL, //(void*) &im, // some random memory
+        &err
+        );
 
     cl::Image2D cl_result = cl::Image2D(
         context,
-         CL_MEM_WRITE_ONLY, // | CL_MEM_COPY_HOST_PTR,
-         cl::ImageFormat(CL_R, CL_UNSIGNED_INT8),  // single channel
-         wd,
-         ht,
-         0,
-         (void*) &im_out, // some random memory
-         &err
-         );
-
-
-    /* 
-    cl::Buffer cl_result = cl::Buffer(
-        context, 
         CL_MEM_WRITE_ONLY, // | CL_MEM_COPY_HOST_PTR,
-        sizeof(unsigned char) * wd * ht //,
-        //im_out
+        cl::ImageFormat(CL_R, CL_UNSIGNED_INT8),  // single channel
+        wd,
+        ht,
+        0,
+        NULL, //(void*) &im, // some random memory
+        &err
         );
-     */
-    //std::cout << "made buffer" << std::endl;
 
-    cl::Kernel kernel(program_, "hello", &err);
-    kernel.setArg(0, cl_image);
-    kernel.setArg(1, cl_result);
+    for (int i = 0; i < 1; i++) {
+      
+      kernel.setArg(0, cl_image);
+      kernel.setArg(1, cl_result);
 
-    cl::NDRange global_size(wd, ht);
-    cl::NDRange local_size(1, 1);
+      cl::NDRange global_size(wd, ht);
+      cl::NDRange local_size(1, 1);
 
-    queue.enqueueWriteImage(
-        cl_image, 
-        CL_TRUE, // blocking write 
-        origin,
-        region,
-        0,0,
-        im //,
-        //NULL,
-        //&event
-        );
-     
-    queue.enqueueNDRangeKernel(
-        kernel, 
-        cl::NullRange,
-        global_size,
-        local_size,
-        NULL,
-        &event); 
+      queue.enqueueWriteImage(
+          cl_image, 
+          CL_TRUE, // blocking write 
+          origin,
+          region,
+          0,0,
+          im //,
+          //NULL,
+          //&event
+          );
+       
+      queue.enqueueNDRangeKernel(
+          kernel, 
+          cl::NullRange,
+          global_size,
+          local_size,
+          NULL,
+          &event); 
 
-    //float
-    queue.enqueueReadImage(
-        cl_result, 
-        CL_TRUE, // blocking read 
-        origin,
-        region,
-        0,0,
-        im_out //,
-        //NULL,
-        //&event
-        );
-           
-    //event.wait();
+      queue.enqueueReadImage(
+          cl_result, 
+          CL_TRUE, // blocking read 
+          origin,
+          region,
+          0,0,
+          im //_out //,
+          //NULL,
+          //&event
+          );
+      
+      //ping = !ping;
+    } // for loop      
  
     std::cout << "output " << std::endl;
     for (int i = 0; i < ht; i++) {
     for (int j = 0; j < ht; j++) {
-      std::cout << "   " << int( im_out[i*wd + j] );
+      std::cout << "   " << int( im[i*wd + j] );
     }
       std::cout << std::endl;
     }
--- src/misc.cl
+++ src/misc.cl
@@ -3,35 +3,16 @@ __constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE |
 
 __kernel void hello(
     __read_only image2d_t image,
-    __read_only image2d_t im_out
-    //__global uint8* result
+    __write_only image2d_t im_out
     )
 { 
   
   const int2 pos = {get_global_id(0), get_global_id(1)};
- 
   uint4 val = read_imageui(image, sampler, pos);
   
-  //const int2 pos2 = {get_global_id(0) + 1, get_global_id(1)};
+  const int2 pos2 = {get_global_id(0) + 1, get_global_id(1)};
+  uint4 val2 = read_imageui(image, sampler, pos2);
 
-  write_imageui(im_out, pos, val);
-  //result[ pos.x + pos.y * get_global_size(0)] = 3;
-  
-  // messing around with writing to output array 
-  //for (uint8 i = 0; i < 16; i++) {
-  //uint8 i = 10;  
-  //result[0] = i;
-  //result[7] = i*2;
-  //result[8] = i*3;
-  //result[15] = i*3;
-  //}
-  //result[get_global_id(0)] = get_global_id(0);
-  //result[get_global_id(1)] = get_global_id(1);
-  /*
-  result[2] = get_work_dim();
-  result[3] = get_global_size(0);
-  result[4] = get_global_size(1);
-  result[5] = 15;
-  */
+  write_imageui(im_out, pos, val - val2/2); // (val + val2)/2 );
 };
 
